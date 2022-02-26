c4c203b7019fbd70325426236c773e06cac0875e
Exanite <exanite78012@gmail.com>
2022-02-26 17:56:51 -0500

    Add wind background sfx

--- Project/Assets/Project.Source/SoundManager.cs
+++ Project/Assets/Project.Source/SoundManager.cs
@@ -1,4 +1,14 @@
+using UnityEngine;
+
 namespace Project.Source
 {
-    public class SoundManager : SingletonBehaviour<SoundManager> {}
+    public class SoundManager : SingletonBehaviour<SoundManager>
+    {
+        public AudioSource soundPrefab;
+
+        public void PlaySound(Vector3 position, AudioClip clip)
+        {
+            Debug.Log($"Playing {clip} at {position}");
+        }
+    }
 }
