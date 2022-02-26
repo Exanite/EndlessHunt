8c461be2db06931643082d21a09c54416e3ed339
Exanite <exanite78012@gmail.com>
2022-02-26 17:19:27 -0500

    Add player movement animations

--- Project/Assets/Project.Source/Player/PlayerMovement.cs
+++ Project/Assets/Project.Source/Player/PlayerMovement.cs
@@ -1,50 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour, PlayerInput.IMovementActions, PlayerInput.IAttackActions
 {
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    
+    [Header("Dependencies")]
+    public Transform rotationTransform;
+    public GameObject bullet;
+    public Animator animator;
+
     [Header("Configuration")]
-    [SerializeField] float moveSpeed = 10f;
-    [SerializeField] float dashSpeed = 10f;
+    [SerializeField]
+    private float moveSpeed = 10f;
+    [SerializeField]
+    private float dashSpeed = 10f;
     public Transform attackPoint;
     public float basicAttackDamage = 1f;
     public float AOEAttackDamage = 1f;
     public float AOERadius = 6f;
     public float AOEOffset = 2f;
-    float health = 10f;
+    private float health = 10f;
 
     [Header("Runtime")]
-    Vector2 movementInput;
-    PlayerInput playerInput;
-    Collider2D myCollider;
-    Rigidbody2D myRigidbody;
-    bool dead = false;
-    public GameObject bullet;
+    public bool dead;
+    
+    // Private
+    private Vector2 movementInput;
+    private PlayerInput playerInput;
+    private Collider2D myCollider;
+    private Rigidbody2D myRigidbody;
 
-    [Header("Dependencies")]
-    public Transform rotationTransform;
-
-    void Awake() 
+    private void Awake()
     {
         playerInput = new PlayerInput();
         playerInput.Movement.SetCallbacks(this);
         playerInput.Attack.SetCallbacks(this);
     }
-    void Start() 
+
+    private void Start()
     {
         myCollider = GetComponent<Collider2D>();
         myRigidbody = GetComponent<Rigidbody2D>();
-        
     }
 
-    void OnEnable()
+    private void OnEnable()
     {
         playerInput.Enable();
     }
 
-    void OnDisable() 
+    private void OnDisable()
     {
         playerInput.Disable();
     }
@@ -57,20 +62,32 @@ public class PlayerMovement : MonoBehaviour, PlayerInput.IMovementActions, Playe
 
     public void OnDash(InputAction.CallbackContext context)
     {
-        if(!context.performed) return;
+        if (!context.performed)
+        {
+            return;
+        }
+
         //Debug.Log("dashing!");
         myRigidbody.AddForce(movementInput * dashSpeed, ForceMode2D.Impulse);
     }
 
     public void OnBasicAttack(InputAction.CallbackContext context)
     {
-        if(dead) return;
-        if(!context.performed) return;
+        if (dead)
+        {
+            return;
+        }
+
+        if (!context.performed)
+        {
+            return;
+        }
+
         //Debug.Log("Shooting!");
         Instantiate(bullet, attackPoint.position, attackPoint.transform.rotation);
         // if(!context.performed) return;
         //     //Debug.Log("Attacking");
-            
+
         // Collider2D[] colliders = Physics2D.OverlapBoxAll(attackPoint.position, new Vector2(1f, 1f), transform.rotation.eulerAngles.z);
         // foreach(Collider2D collider in colliders) 
         // {
@@ -84,15 +101,19 @@ public class PlayerMovement : MonoBehaviour, PlayerInput.IMovementActions, Playe
 
     public void OnAOEAttack(InputAction.CallbackContext context)
     {
-        if(!context.performed) return;
-            Debug.Log("AOE Attack!");
-
-        for(float i = AOERadius; i > 0; i-=AOEOffset) 
+        if (!context.performed)
         {
-            Collider2D[] colliders = Physics2D.OverlapBoxAll(attackPoint.position, new Vector2(i, i), transform.rotation.eulerAngles.z);
-            foreach(Collider2D collider in colliders) 
+            return;
+        }
+
+        Debug.Log("AOE Attack!");
+
+        for (var i = AOERadius; i > 0; i -= AOEOffset)
+        {
+            var colliders = Physics2D.OverlapBoxAll(attackPoint.position, new Vector2(i, i), transform.rotation.eulerAngles.z);
+            foreach (var collider in colliders)
             {
-                if(collider.TryGetComponent(out EnemyController enemyController)) 
+                if (collider.TryGetComponent(out EnemyController enemyController))
                 {
                     enemyController.takeDamage(AOEAttackDamage);
                     Debug.LogWarning("AOE DAMAGE " + i);
@@ -104,18 +125,21 @@ public class PlayerMovement : MonoBehaviour, PlayerInput.IMovementActions, Playe
     public void takeDamage(float damageTaken)
     {
         health -= damageTaken;
-        if(health <= 0)
+        if (health <= 0)
         {
             Debug.Log("im dead :(");
             dead = true;
         }
         else
+        {
             Debug.Log("Damage taken! health: " + health);
-    }    
+        }
+    }
 
-    void FixedUpdate() 
+    private void FixedUpdate()
     {
         myRigidbody.AddForce(movementInput * moveSpeed);
+        animator.SetBool(IsWalking, movementInput.sqrMagnitude > 0.1f);
     }
 
     public float getBasicAttackDamage()
