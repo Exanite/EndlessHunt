4470a2cbcc9180cda06f590850e39e518c2a74be
Exanite <exanite78012@gmail.com>
2022-02-26 18:08:35 -0500

    Add Dodge sound and implement SoundManager

--- Project/Assets/Project.Source/SoundManager.cs
+++ Project/Assets/Project.Source/SoundManager.cs
@@ -4,11 +4,19 @@ namespace Project.Source
 {
     public class SoundManager : SingletonBehaviour<SoundManager>
     {
+        public Camera mainCamera;
         public AudioSource soundPrefab;
 
-        public void PlaySound(Vector3 position, AudioClip clip)
+        public AudioSource PlaySound(AudioClip clip, Vector3 position, float volume = 1)
         {
-            Debug.Log($"Playing {clip} at {position}");
+            Debug.Log($"Playing {clip}");
+
+            var sound = Instantiate(soundPrefab);
+            sound.clip = clip;
+            sound.transform.position = position;
+            sound.volume = volume;
+
+            return sound;
         }
     }
 }
--- Project/Assets/Project/Audio/README.md
+++ Project/Assets/Project/Audio/README.md
@@ -1,9 +1,15 @@
-Audio from https://opengameart.org/
+# Audio Credits
 
-(Should remove unused later)
+## From https://opengameart.org/
+
+>(Todo: remove unused later)
+
+### Used
 
 https://opengameart.org/content/wind1
 https://opengameart.org/content/3-melee-sounds
+
+### Unused
 https://opengameart.org/content/16-button-clicks
 https://opengameart.org/content/swishes-sound-pack
 https://opengameart.org/content/different-steps-on-wood-stone-leaves-gravel-and-mud
