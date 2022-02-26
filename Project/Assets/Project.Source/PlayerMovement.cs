e868965e66be4bb6a1d1b50dcc3f0fcadc8737e0
rpgroves <grovesrpg@gmail.com>
2022-02-25 20:20:14 -0500

    Movement Script

---
+++ Project/Assets/Project.Source/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour, GameInput.IPlayerActions
+{
+    [SerializeField] float moveSpeed;
+    GameInput inputClass;
+    Collider2D myCollider;
+    Rigidbody2D myRigidbody;
+
+    void Awake() 
+    {
+        inputClass = new GameInput();
+        inputClass.Player.SetCallbacks(this);
+    }
+    void Start() 
+    {
+        myCollider = GetComponent<Collider2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void OnEnable()
+    {
+        inputClass.Enable();
+    }
+
+    void OnDisable() 
+    {
+        inputClass.Disable();
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        //Debug.Log("moving!");
+        Vector2 movement = context.ReadValue<Vector2>();
+        myRigidbody.AddForce(movement);
+    }
+}
