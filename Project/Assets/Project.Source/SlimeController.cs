ad942b728a3a2c62cafe7c88fc3f189c6c72f495
rpgroves <grovesrpg@gmail.com>
2022-02-27 13:52:26 -0500

    player sound effect is now local to the prefab

--- Project/Assets/Project.Source/SlimeController.cs
+++ Project/Assets/Project.Source/SlimeController.cs
@@ -17,24 +17,24 @@ public class SlimeController : MonoBehaviour
 
     void FixedUpdate()
     {
-        
+
     }
 
     void UpdateMovementSpeed()
     {
-        var offset = target.transform.position - transform.position;
-        var direction = offset.normalized;
-        movement = direction;
+        // var offset = target.transform.position - transform.position;
+        // var direction = offset.normalized;
+        // movement = direction;
         
-        if (GetDistanceToTarget() < stopDistance)
-        {
-            movement = Vector2.zero;
-            if(timer < 0) 
-            {
-                //Debug.Log("attack!");
-                BulletAttack();
-                timer = cooldown;
-            }
-        }
+        // if (GetDistanceToTarget() < stopDistance)
+        // {
+        //     movement = Vector2.zero;
+        //     if(timer < 0) 
+        //     {
+        //         //Debug.Log("attack!");
+        //         BulletAttack();
+        //         timer = cooldown;
+        //     }
+        // }
     }
 }
