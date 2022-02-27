dc3d26f72f21dcbf219b230b6b1d6dc44a27a778
Exanite <exanite78012@gmail.com>
2022-02-27 15:07:48 -0500

    Add ThemeScene fade in and fade out

---
+++ Project/Assets/Project.Source/UI/ThemeScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ThemeScene : MonoBehaviour
+{
+    public string nextScene;
+
+    private IEnumerator Start()
+    {
+        yield return FadeTransition.Instance.FadeToClear(2f);
+        yield return FadeTransition.Instance.FadeToBlack(2f);
+
+        SceneManager.LoadScene(nextScene);
+    }
+}
