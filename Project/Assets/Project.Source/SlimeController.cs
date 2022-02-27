460ed1edd2d72696bc0b2263c09e14b6a9452cd0
rpgroves <grovesrpg@gmail.com>
2022-02-27 13:48:58 -0500

    pushing run noise

--- Project/Assets/Project.Source/SlimeController.cs
+++ Project/Assets/Project.Source/SlimeController.cs
@@ -14,4 +14,27 @@ public class SlimeController : MonoBehaviour
     {
         
     }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void UpdateMovementSpeed()
+    {
+        var offset = target.transform.position - transform.position;
+        var direction = offset.normalized;
+        movement = direction;
+        
+        if (GetDistanceToTarget() < stopDistance)
+        {
+            movement = Vector2.zero;
+            if(timer < 0) 
+            {
+                //Debug.Log("attack!");
+                BulletAttack();
+                timer = cooldown;
+            }
+        }
+    }
 }
