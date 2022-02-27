f9a5b25882349925b733075faa602fa62dd764c4
rpgroves <grovesrpg@gmail.com>
2022-02-27 15:45:29 -0500

    fades + transitions

--- Project/Assets/Project.Source/LeaveMenu.cs
+++ Project/Assets/Project.Source/LeaveMenu.cs
@@ -1,10 +1,11 @@
-
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LeaveMenu : MonoBehaviour
 {
+    public string nextScene = "";
     public FadeTransition fadeTransition;
     public float duration = .5f;
     void Start()
@@ -24,5 +25,7 @@ public class LeaveMenu : MonoBehaviour
     private IEnumerator Fade()
     {
         yield return FadeTransition.Instance.FadeToBlack(duration);
+        yield return new WaitForSeconds(0.2f);
+        SceneManager.LoadScene(nextScene);
     }
 }
