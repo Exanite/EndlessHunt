3988e67975fbf304b07adc6ccf85ffc27d733836
Exanite <exanite78012@gmail.com>
2022-03-05 18:29:40 -0500

    Fix projectiles not dealing damage if they spawn inside a collider

--- Project/Assets/Project.Source/Gameplay/Projectile.cs
+++ Project/Assets/Project.Source/Gameplay/Projectile.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Project.Source.Gameplay
@@ -14,10 +16,14 @@ namespace Project.Source.Gameplay
         public float speed;
         public float distanceRemaining;
         public float lifetime;
-
-        private bool hasInitialized;
-
+        
         private Rigidbody2D rb;
+        private Queue<Collider2D> colliderQueue;
+
+        private void Awake()
+        {
+            colliderQueue = new Queue<Collider2D>();
+        }
 
         private void Start()
         {
@@ -29,12 +35,15 @@ namespace Project.Source.Gameplay
             damage = owner.projectileDamage;
             distanceRemaining = owner.projectileMaxDistance;
             lifetime = owner.projectileLifetime;
-
-            hasInitialized = true;
         }
 
         private void FixedUpdate()
         {
+            while (colliderQueue.TryDequeue(out var collider))
+            {
+                OnCollide(collider);
+            }
+            
             rb.velocity = transform.right * speed;
 
             var distanceTraveled = rb.velocity.magnitude * Time.deltaTime;
@@ -48,11 +57,11 @@ namespace Project.Source.Gameplay
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (!hasInitialized)
-            {
-                return;
-            }
+            colliderQueue.Enqueue(other);
+        }
 
+        private void OnCollide(Collider2D other)
+        {
             if (other.attachedRigidbody)
             {
                 if (faction == Faction.Enemy && other.attachedRigidbody.TryGetComponent(out Player player))
